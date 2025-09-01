8f424f1670470a6b9f7422077f2a7339b2c60f87
Phương Nhi <cpn2003.py@gmail.com>
2025-09-01 10:35:15 +0700

    [Hotfix][BE] Update API to return error if reported item is deleted or not found. Add API for bulk deletion of user reading history.

--- Application/Features/ReadingProcess/Command/DeleteReadingProcessCommand.cs
+++ Application/Features/ReadingProcess/Command/DeleteReadingProcessCommand.cs
@@ -1,11 +1,6 @@
 ﻿using Infrastructure.Repositories.Interfaces;
 using MediatR;
 using Shared.Contracts.Response;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Features.ReadingProcess.Command
 {
